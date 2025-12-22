23fe5ef6ad1496b2b303e53d5ba3bf54a3308ebb
Tornike Kapanadze <tornikekapan@gmail.com>
2025-12-22 17:00:13 +0200

    fix: restore natve messaging key

--- src/constants/nativeMessaging.js
+++ src/constants/nativeMessaging.js
@@ -3,4 +3,4 @@ export const EXTENSION_ID = 'pdeffakfmcdnjjafophphgmddmigpejh'
 export const MANIFEST_NAME = 'com.pears.pass'
 
 export const NATIVE_MESSAGING_BRIDGE_PEAR_LINK =
-  'pear://pd6iat7w4dznakx9gydco1ncaccwr6uncan16e3e8xeifwysmwfy'
+  'pear://i49831s3quatekogbc411cdfmg6xmjt1dycxxr3kt1b1qms5x8ro'
